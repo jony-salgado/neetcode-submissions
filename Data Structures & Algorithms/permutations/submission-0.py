eaacce5010549d4cabd5969870e71e7c4612d97b
jony-salgado <jonysalgadofilho@gmail.com>
2026-08-27 19:38:38 -0300

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+
+        def dfs(curr):
+            if len(curr) == len(nums):
+                ans.append(curr.copy())
+
+
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    dfs(curr)
+                    curr.pop()
+
+        dfs([])
+        return ans
